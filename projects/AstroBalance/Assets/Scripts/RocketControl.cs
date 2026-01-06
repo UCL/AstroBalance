fb3672a09529a793a10f0cd8ac7cb9393103389d
Stephen Thompson <s.thompson@ucl.ac.uk>
2026-01-06 11:58:50 +0000

    Implementing gaze tracking using gaze buffer

--- projects/AstroBalance/Assets/Scripts/RocketControl.cs
+++ projects/AstroBalance/Assets/Scripts/RocketControl.cs
@@ -1,20 +1,47 @@
 using UnityEngine;
+using Tobii.GameIntegration.Net;
+using GazeBuffer;
+
 
 public class rocket_control : MonoBehaviour
 {
     Tracker tracker;
+
+    [SerializeField, Tooltip("Set to true to substitute the mouse for the eye tracker (for debugging purposes)")]
+    private bool useMouseForTracker = false;
+
+    [SerializeField, Tooltip("The capacity of the gaze buffer to use.")]
+    private int gazeBufferCapacity = 100;
+
+
+    private gazeBuffer gazeBuffer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tracker = FindFirstObjectByType<Tracker>();
+        gazeBuffer = new gazeBuffer(gazeBufferCapacity, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var gp = tracker.getGazePoint();
-	if gp.timestamp is recent
+        GazePoint gp = new GazePoint();
+        if (useMouseForTracker)
+        {
+            var mousePos = Input.mousePosition;
+            gp.X = mousePos.x;
+            gp.Y = mousePos.y;
+            gp.TimeStampMicroSeconds = (long)(Time.timeSinceLevelLoad * 1000000);
+        }
+        else
+        {
+            gp = tracker.getGazePoint();
+        }
         transform.Translate(new Vector3(gp.X, gp.Y, 0f));
+        gazeBuffer.Add(gp);
+        Debug.Log(gazeBuffer.Size + " : " + gazeBuffer.Get().X);
+
         // Debug.Log("Rocket control update" + TrackerInterface.getGazePoint()[0]);
     }
 }
