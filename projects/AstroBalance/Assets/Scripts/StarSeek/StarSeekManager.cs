7aaac30ac480e5ce2ac169c0e70467db1f078579
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-08 12:53:40 +0000

    add countdown timer to star seek

--- projects/AstroBalance/Assets/Scripts/StarSeek/StarSeekManager.cs
+++ projects/AstroBalance/Assets/Scripts/StarSeek/StarSeekManager.cs
@@ -5,24 +5,44 @@ public class StarSeekManager : MonoBehaviour
 {
     [SerializeField, Tooltip("Text mesh pro object for score text")]
     private TextMeshProUGUI scoreText;
-    [SerializeField, Tooltip("Text mesh pro object for countdown timer text")]
-    private TextMeshProUGUI timerText;
+    [SerializeField, Tooltip("Countdown timer prefab")]
+    private CountdownTimer timer;
     [SerializeField, Tooltip("Screen shown upon winning the game")]
     private GameObject winScreen;
     [SerializeField, Tooltip("Game time limit in seconds")]
     private int timeLimit = 120;
 
     private int score;
+    private TextMeshProUGUI winText;
+    private bool gameActive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        winText = winScreen.GetComponentInChildren<TextMeshProUGUI>();
+        score = 0;
+        scoreText.text = score.ToString();
+        timer.StartCountdown(timeLimit);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If time limit reached, end game
+        if (timer.GetTimeRemaining() <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        if (gameActive)
+        {
+            gameActive = false;
+
+            winText.text = "Congratulations! \n \n You collected " + score + " stars";
+            winScreen.SetActive(true);
+        }
     }
 }
