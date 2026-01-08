3c474a62f662e3352e62674d8512b54a02cb7f85
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-08 11:32:12 +0000

    sparkle then collect star after n seconds

--- projects/AstroBalance/Assets/Scripts/StarSeek/LockedOn.cs
+++ projects/AstroBalance/Assets/Scripts/StarSeek/LockedOn.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class LockedOn : MonoBehaviour
 {
+    [SerializeField, Tooltip("Number of seconds required to collect star")]
+    private int doubleLockTime = 2;
+    [SerializeField, Tooltip("Particle system to be shown on star collection")]
+    private GameObject collectEffect;
+    [SerializeField, Tooltip("Particle system to be shown on star double lock")]
+    private GameObject lockedEffect;
+    [SerializeField, Tooltip("Bloom intensity for single lock")]
+    private float singleLockBloom = 10f;
+    [SerializeField, Tooltip("Bloom intensity for double lock")]
+    private float doubleLockBloom = 20f;
+
     private string gazeCrosshairName = "GazeCrosshair";
     private string poseCrosshairName = "PoseCrosshair";
 
     private SpriteRenderer sprite;
+    private Bloom bloom;
     private Color defaultColor = Color.white;
+    private GameObject doubleLockSparkle;
     private Color singleLockColor = Color.red;
     private Color doubleLockColor = Color.cyan;
 
     private LockStatus lockStatus = LockStatus.None;
+    private float doubleLockStart;
     private enum LockStatus
     {
         None,
@@ -23,12 +39,24 @@ public class LockedOn : MonoBehaviour
     {
         sprite = GetComponent<SpriteRenderer>();
         sprite.color = defaultColor;
-        
+
+        Volume globalVolume = FindFirstObjectByType<Volume>();
+        globalVolume.profile.TryGet(out bloom);
+        bloom.intensity.value = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (lockStatus == LockStatus.Double && (Time.time - doubleLockStart >= doubleLockTime))
+        {
+            // collect star
+            Destroy(doubleLockSparkle);
+            GameObject collectedSparkle = Instantiate<GameObject>(collectEffect);
+            collectedSparkle.transform.position = transform.position;
+            Destroy(collectedSparkle, 1.0f);
+            Destroy(gameObject);
+        }
         
     }
 
@@ -37,13 +65,17 @@ public class LockedOn : MonoBehaviour
         Debug.Log(other.gameObject.name);
         if (other.gameObject.name == gazeCrosshairName && lockStatus == LockStatus.None)
         {
-            sprite.color = singleLockColor;
+            //sprite.color = singleLockColor;
             lockStatus = LockStatus.Single;
+            bloom.intensity.value = singleLockBloom;
         }
         else if (other.gameObject.name == poseCrosshairName && lockStatus == LockStatus.Single)
         {
-            sprite.color = doubleLockColor;
+            //sprite.color = doubleLockColor;
             lockStatus = LockStatus.Double;
+            doubleLockStart = Time.time;
+            bloom.intensity.value = doubleLockBloom;
+            doubleLockSparkle = Instantiate<GameObject>(lockedEffect, transform.position, Quaternion.identity);
         }
     }
 
@@ -51,12 +83,15 @@ public class LockedOn : MonoBehaviour
     {
         if (other.gameObject.name == gazeCrosshairName && lockStatus != LockStatus.None)
         {
-            sprite.color = defaultColor;
+            //sprite.color = defaultColor;
             lockStatus = LockStatus.None;
+            bloom.intensity.value = 0f;
         } else if (other.gameObject.name == poseCrosshairName && lockStatus == LockStatus.Double)
         {
-            sprite.color = singleLockColor;
+            //sprite.color = singleLockColor;
             lockStatus = LockStatus.Single;
+            bloom.intensity.value = singleLockBloom;
+            Destroy(doubleLockSparkle);
         }
     }
 }
