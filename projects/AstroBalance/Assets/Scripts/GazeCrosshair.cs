4d11762d0ba9ce5ce1a51e60ec2d8dc150c4d435
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-16 11:27:38 +0000

    minor formatting changes to GazeCrosshair

--- projects/AstroBalance/Assets/Scripts/GazeCrosshair.cs
+++ projects/AstroBalance/Assets/Scripts/GazeCrosshair.cs
@@ -4,7 +4,6 @@ public class GazeCrosshair : MonoBehaviour
 {
     private Tracker tracker;
 
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
