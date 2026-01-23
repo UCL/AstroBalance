af28b5725f55486c6058bdef5f0d0bc61ea6f35d
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-23 12:46:37 +0000

    add sway line and link to head x movement

---
+++ projects/AstroBalance/Assets/Scripts/ZeroGravity/SwayLine.cs
@@ -0,0 +1,27 @@
+using Tobii.GameIntegration.Net;
+using UnityEngine;
+
+public class SwayLine : MonoBehaviour
+{
+    [SerializeField, Tooltip("Head x movement scaling (moving 1mm, moves this many unity units)")]
+    private float headXScaling = 0.01f;
+
+    private Tracker tracker;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tracker = FindFirstObjectByType<Tracker>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HeadPose currentHeadPose = tracker.getHeadPose();
+        float xPosMm = currentHeadPose.Position.X;
+
+        // We only move the sway line on the x axis - left/right (we don't care about
+        // changes in head height, or distance from screen)
+        transform.position = new Vector3(xPosMm * headXScaling, transform.position.y, 0);
+    }
+}
