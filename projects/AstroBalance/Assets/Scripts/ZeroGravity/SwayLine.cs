a4fe16e4975500e7601bb19dc016e422f2dc3891
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-23 14:24:39 +0000

    colour change on exceeding range

--- projects/AstroBalance/Assets/Scripts/ZeroGravity/SwayLine.cs
+++ projects/AstroBalance/Assets/Scripts/ZeroGravity/SwayLine.cs
@@ -5,13 +5,21 @@ public class SwayLine : MonoBehaviour
 {
     [SerializeField, Tooltip("Head x movement scaling (moving 1mm, moves this many unity units)")]
     private float headXScaling = 0.01f;
+    [SerializeField, Tooltip("Head x movement tolerance (mm) - beyond this limit scoring stops")]
+    private float headXTolerance = 100;
+    [SerializeField, Tooltip("Inside x range colour")]
+    private Color inRangeColor = Color.white;
+    [SerializeField, Tooltip("Outside x range colour")]
+    private Color outRangeColor = Color.black;
 
     private Tracker tracker;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tracker = FindFirstObjectByType<Tracker>();   
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,6 +29,14 @@ public class SwayLine : MonoBehaviour
         float xPosMm = currentHeadPose.Position.X;
         float rollDegrees = currentHeadPose.Rotation.RollDegrees;
 
+        if (Mathf.Abs(xPosMm) >= headXTolerance)
+        {
+            spriteRenderer.color = outRangeColor;
+        } else
+        {
+            spriteRenderer.color = inRangeColor;
+        }
+        
         // We only move the sway line on the x axis - left/right (we don't care about
         // changes in head height, or distance from screen)
         transform.position = new Vector3(xPosMm * headXScaling, transform.position.y, 0);
