f2433135f4b5d2d046e9a85633dba9b797f0c111
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-26 11:18:23 +0000

    fix issue with countdown not appearing in built game

--- projects/AstroBalance/Assets/Scripts/ZeroGravity/PoseAvatar.cs
+++ projects/AstroBalance/Assets/Scripts/ZeroGravity/PoseAvatar.cs
@@ -16,10 +16,14 @@ public class PoseAvatar : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private int currentIndex = -1;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
