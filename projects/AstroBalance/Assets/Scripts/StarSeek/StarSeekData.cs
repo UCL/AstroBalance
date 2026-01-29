ccdf0b006424475fbe75c5ffa00effbf73660c63
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-01-29 10:41:34 +0000

    add data saving for star seek

---
+++ projects/AstroBalance/Assets/Scripts/StarSeek/StarSeekData.cs
@@ -0,0 +1,10 @@
+
+/// <summary>
+/// Save data for a single star seek session
+/// </summary>
+[System.Serializable]
+public class StarSeekData : GameData
+{
+    public int timeLimitSeconds;
+    public int nStarsCollected;
+}
