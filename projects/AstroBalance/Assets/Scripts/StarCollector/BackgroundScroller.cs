b0d82a48b35a8d688976a3741f9d98d2cda7da04
mmcleod89 <michael.mcleod@ucl.ac.uk>
2026-03-04 09:58:55 +0000

    Creating a clean graphics update  (#62)
    * Add prefabs/scripts and update star collector
    
    * Update Star Collector scene file
    
    * Update Rocket Launch
    
    * Update Space walk
    
    * Update Star map
    
    * Updating exit buttons
    
    * Partial star seek update
    
    * Update Star seek
    
    * Update zero gravity
    
    * Update menu screen
    
    * Remove unused prefabs
    
    * Relinking buttons
    
    * Fix buttons
    
    * Update to softer backgrounds

---
+++ projects/AstroBalance/Assets/Scripts/StarCollector/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField, Tooltip("Background Scrolling Rate")]
+    private float speed = 1.0f;
+
+    private Material mat;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time;
+        mat.mainTextureOffset = new Vector2(0, speed * t);
+    }
+}
