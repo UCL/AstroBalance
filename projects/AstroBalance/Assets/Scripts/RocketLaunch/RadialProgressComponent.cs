18607fe543d20d0c2f3cb7c55b2f3aec5b3f8c93
Stephen Thompson <s.thompson@ucl.ac.uk>
2026-03-31 09:53:40 +0100

    Trying to get UI scaling right with some help from Claude sonnet 4.6

--- projects/AstroBalance/Assets/Scripts/RocketLaunch/RadialProgressComponent.cs
+++ projects/AstroBalance/Assets/Scripts/RocketLaunch/RadialProgressComponent.cs
@@ -9,26 +9,39 @@ public class RadialProgressComponent : MonoBehaviour
 {
     LaunchControl countdownController;
     RadialProgress m_RadialProgress;
+    VisualElement m_Container;
 
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
         countdownController = FindFirstObjectByType<LaunchControl>();
+
+        // A full-screen container with an explicit 100%x100% size gives child elements
+        // a definite reference for percentage-based top/left, which otherwise fails to
+        // resolve when display scaling is active (panel logical size != Screen pixels).
+        m_Container = new VisualElement();
+        m_Container.style.position = Position.Absolute;
+        m_Container.style.left = 0;
+        m_Container.style.top = 0;
+        m_Container.style.width = new Length(100, LengthUnit.Percent);
+        m_Container.style.height = new Length(100, LengthUnit.Percent);
+
         m_RadialProgress = new RadialProgress()
         {
             style =
             {
                 position = Position.Absolute,
                 left = new Length(45, LengthUnit.Percent),
-                //top = new Length(20, LengthUnit.Percent), // this doesn't seem to work for setting the height.
-		top = 225,
+                // top = new Length(100, LengthUnit.Percent),
+		top = 300,
                 width = new Length(10, LengthUnit.Percent),
-                height = new Length(10, LengthUnit.Percent)
+                height = new Length(10, LengthUnit.Percent),
             },
         };
         m_RadialProgress.trackColour = Color.red;
 
-        root.Add(m_RadialProgress);
+        m_Container.Add(m_RadialProgress);
+        root.Add(m_Container);
     }
 
     void Update()
@@ -37,7 +50,7 @@ public class RadialProgressComponent : MonoBehaviour
         if (countdownController.GetProgress() >= 100)
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
-            root.Remove(m_RadialProgress);
+            root.Remove(m_Container);
             this.enabled = false;
         }
     }
