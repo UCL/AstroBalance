2ec60e1101022666365eceb29e1ae1da41c04063
Stephen Thompson <s.thompson@ucl.ac.uk>
2026-04-01 14:36:16 +0100

    Trying to get the height scaling right

--- projects/AstroBalance/Assets/Scripts/RocketLaunch/RadialProgressComponent.cs
+++ projects/AstroBalance/Assets/Scripts/RocketLaunch/RadialProgressComponent.cs
@@ -31,14 +31,20 @@ public class RadialProgressComponent : MonoBehaviour
             style =
             {
                 position = Position.Absolute,
-                left = new Length(45, LengthUnit.Percent),
-                // top = new Length(100, LengthUnit.Percent),
-		top = 300,
-                width = new Length(10, LengthUnit.Percent),
-                height = new Length(10, LengthUnit.Percent),
+                left = new Length(44, LengthUnit.Percent),
+                width = new Length(12, LengthUnit.Percent),
+                height = new Length(12, LengthUnit.Percent),
             },
         };
         m_RadialProgress.trackColour = Color.red;
+
+	// Claude (Sonnet 4.6) came up with the following in order to get the height right. It works better than
+	// anything else I've found but is still not quite right.
+        // ScaleWithScreenSize + match=0 (width): logical panel height = Screen.height * refWidth / Screen.width.
+        // Display scaling cancels out because both Screen dimensions scale by the same factor.
+        var panelSettings = GetComponent<UIDocument>().panelSettings;
+        float panelHeight = Screen.height * panelSettings.referenceResolution.x / Screen.width;
+        m_RadialProgress.style.top = panelHeight * 0.480f;
 
         m_Container.Add(m_RadialProgress);
         root.Add(m_Container);
