c7cdfcb0c0aba353e58b519c18e20e3277c7c0f8
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-05-18 15:01:42 +0100

    start drafting badge prefab

---
+++ projects/AstroBalance/Assets/Scripts/Badges/Badge.cs
@@ -0,0 +1,61 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Badge : MonoBehaviour
+{
+    [SerializeField, Tooltip("Badge image")]
+    private Image badgeImage;
+
+    [SerializeField, Tooltip("Game number text")]
+    private TextMeshProUGUI gameNumberText;
+
+    [SerializeField, Tooltip("Default badge sprite")]
+    private Sprite defaultSprite;
+
+    [SerializeField, Tooltip("Bronze badge sprite")]
+    private Sprite bronzeSprite;
+
+    [SerializeField, Tooltip("Silver badge sprite")]
+    private Sprite silverSprite;
+
+    [SerializeField, Tooltip("Gold badge sprite")]
+    private Sprite goldSprite;
+
+    [SerializeField, Tooltip("Number of complete games required for bronze")]
+    private int nGamesBronze = 10;
+
+    [SerializeField, Tooltip("Nubmer of complete games required for silver")]
+    private int nGamesSilver = 20;
+
+    [SerializeField, Tooltip("Number of complete games required for gold")]
+    private int nGamesGold = 30;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        badgeImage.sprite = defaultSprite;
+    }
+
+    public void SetCompleteGames(int nGames)
+    {
+        if (nGames >= nGamesGold)
+        {
+            badgeImage.sprite = goldSprite;
+        }
+        else if (nGames >= nGamesSilver)
+        {
+            badgeImage.sprite = silverSprite;
+        }
+        else if (nGames >= nGamesBronze)
+        {
+            badgeImage.sprite = bronzeSprite;
+        }
+        else
+        {
+            badgeImage.sprite = defaultSprite;
+        }
+
+        gameNumberText.text = nGames.ToString();
+    }
+}
---
+++ projects/AstroBalance/Assets/Scripts/Badges/BadgeManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BadgeManager : MonoBehaviour
+{
+    void Awake() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
