b422ef23a9799eda18ba8be5d51002b2101dfb2a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-05-27 11:36:05 +0100

    translate camera from base to top of background

--- projects/AstroBalance/Assets/Scripts/SplashScreen/SplashAnimation.cs
+++ projects/AstroBalance/Assets/Scripts/SplashScreen/SplashAnimation.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -10,7 +11,11 @@ public class SplashAnimation : MonoBehaviour
     [SerializeField, Tooltip("Rocket game object")]
     private GameObject rocket;
 
+    [SerializeField, Tooltip("Speed to move the camera at")]
+    private float cameraMoveSpeed = 1f;
+
     private Camera cam;
+    private float maxCameraYPos;
 
     void Awake()
     {
@@ -26,11 +31,30 @@ public class SplashAnimation : MonoBehaviour
         Vector3 cameraPosition = cam.transform.position;
         float yPos = background.bounds.min.y + cam.orthographicSize;
         cam.transform.position = new Vector3(cameraPosition.x, yPos, cameraPosition.z);
+
+        // Find max camera position (where aligned with top of background)
+        maxCameraYPos = background.bounds.max.y - cam.orthographicSize;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() { }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        Vector3 camPosition = cam.transform.position;
+
+        if (camPosition.y < maxCameraYPos)
+        {
+            Vector3 yTranslate = Vector3.up * cameraMoveSpeed * Time.deltaTime;
+            if ((camPosition + yTranslate).y > maxCameraYPos)
+            {
+                cam.transform.position = new Vector3(camPosition.x, maxCameraYPos, camPosition.z);
+            }
+            else
+            {
+                cam.transform.Translate(yTranslate, Space.World);
+            }
+        }
+    }
 }
